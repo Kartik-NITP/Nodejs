f5bd6999bcf5a9cc012c84e0f09d932e567f842c
Kartik Sihotiya <ksihotiya@gmail.com>
2020-10-19 16:15:28 +0530

    First Assignment

--- node-express/routes/leaderRouter.js
+++ node-express/routes/leaderRouter.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const dishRouter = express.Router();
-
-dishRouter.use(bodyParser.json())
-dishRouter.route('/')
+const leaderRouter = express.Router();
+leaderRouter.use(bodyParser.json());
+leaderRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -21,8 +20,8 @@ dishRouter.route('/')
 })
 .delete((req, res, next) => {
     res.end('Deleting all dishes');
-})
-dishRouter.route('./:dishId')
+});
+leaderRouter.route('/:leaderId')
 .get((req,res,next) => {
     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 })
@@ -38,5 +37,4 @@ dishRouter.route('./:dishId')
 .delete((req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 })
-
-module.exports = dishRouter;
+module.exports = leaderRouter;
--- node-express/routes/promoRouter.js
+++ node-express/routes/promoRouter.js
@@ -1,9 +1,8 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const dishRouter = express.Router();
-
-dishRouter.use(bodyParser.json())
-dishRouter.route('/')
+const promoRouter = express.Router();
+promoRouter.use(bodyParser.json());
+promoRouter.route('/')
 .all((req,res,next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -21,8 +20,8 @@ dishRouter.route('/')
 })
 .delete((req, res, next) => {
     res.end('Deleting all dishes');
-})
-dishRouter.route('./:dishId')
+});
+promoRouter.route('/:promotionId')
 .get((req,res,next) => {
     res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
 })
@@ -38,5 +37,4 @@ dishRouter.route('./:dishId')
 .delete((req, res, next) => {
     res.end('Deleting dish: ' + req.params.dishId);
 })
-
-module.exports = dishRouter;
+module.exports = promoRouter;
