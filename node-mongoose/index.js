25a582dcc44c63ca2b121bd2cbe8b9f87553b74f
Kartik Sihotiya <ksihotiya@gmail.com>
2020-10-25 15:57:58 +0530

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -4,14 +4,13 @@ const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 connect.then((db)=> {
     console.log("Connected correctly to server");
-    var newDish = Dishes({
+    Dishes.create({
         name:'Uthapizza',
         description:'test'
-    });
-    newDish.save()
+    })
     .then((dish)=>{
         console.log(dish);
-        return Dishes.find({});
+        return Dishes.find({}).exec();
     })
     .then((dishes)=>{
         console.log(dishes);
